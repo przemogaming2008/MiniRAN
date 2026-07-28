a2f9596b81b07c1d813b07c750c40f0d3edaf5b3
Wojciech Gudz <wojtek.gudz@interia.eu>
2026-07-28 16:20:46 +0200

    test config: add negative parser coverage

--- tests/unit/test_config_invalid_numeric_value.cpp
+++ tests/unit/test_config_invalid_numeric_value.cpp
@@ -7,19 +7,95 @@
 
 using namespace miniran;
 
-TEST_CASE(config_invalid_numeric_value_returns_error) {
-    const std::string path = "tmp_invalid_numeric_config.cfg";
+namespace {
 
-    {
-        std::ofstream file(path);
-        file << "step_ms = abc\n";
-    }
+void writeFile(const std::string& path, const std::string& content) {
+    std::ofstream file(path);
+    file << content;
+}
+
+void assertConfigFails(const std::string& path, const std::string& content, const std::string& expectedErrorPart) {
+    writeFile(path, content);
 
     std::string error;
-    auto config = ScenarioConfig::fromFile(path, error);
+    const auto config = ScenarioConfig::fromFile(path, error);
 
     std::remove(path.c_str());
 
     ASSERT_TRUE(!config.has_value());
-    ASSERT_TRUE(error.find("step_ms") != std::string::npos);
+    ASSERT_TRUE(error.find(expectedErrorPart) != std::string::npos);
+}
+
+}  // namespace
+
+TEST_CASE(config_invalid_text_numeric_value_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_numeric_text.cfg",
+        "step_ms = abc\n",
+        "step_ms"
+    );
+}
+
+TEST_CASE(config_numeric_value_with_trailing_text_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_numeric_trailing.cfg",
+        "step_ms = 10junk\n",
+        "step_ms"
+    );
+}
+
+TEST_CASE(config_negative_unsigned_value_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_numeric_negative.cfg",
+        "step_ms = -1\n",
+        "step_ms"
+    );
+}
+
+TEST_CASE(config_uint32_overflow_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_uint32_overflow.cfg",
+        "ue_id = 4294967297\n",
+        "ue_id"
+    );
+}
+
+TEST_CASE(config_zero_step_ms_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_zero_step.cfg",
+        "step_ms = 0\n",
+        "step_ms"
+    );
+}
+
+TEST_CASE(config_unknown_key_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_unknown_key.cfg",
+        "typo_step_ms = 10\n",
+        "Unknown config key"
+    );
+}
+
+TEST_CASE(config_duplicate_key_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_duplicate_key.cfg",
+        "step_ms = 10\nstep_ms = 20\n",
+        "Duplicate config key"
+    );
+}
+
+TEST_CASE(config_invalid_transport_mode_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_transport_mode.cfg",
+        "transport_mode = spaceship\n",
+        "transport_mode"
+    );
+}
+
+TEST_CASE(config_invalid_traffic_pattern_returns_error) {
+    assertConfigFails(
+        "tmp_invalid_traffic_pattern.cfg",
+        "traffic_pattern = randomstorm\n",
+        "traffic_pattern"
+    );
 }
