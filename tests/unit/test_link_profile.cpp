b1da5882b0f7f30d8416cdd0a3dff4f4bf5e705f
Wojciech Gudz <wojtek.gudz@interia.eu>
2026-07-28 17:29:42 +0200

    fix transport: reject extreme delay profiles

--- tests/unit/test_link_profile.cpp
+++ tests/unit/test_link_profile.cpp
@@ -1,4 +1,9 @@
+#include <cstdint>
+#include <limits>
+
+#include "miniran/transport/datagram.h"
 #include "miniran/transport/link_profile.h"
+#include "miniran/transport/virtual_network.h"
 #include "support/test_framework.h"
 
 using namespace miniran;
@@ -18,4 +23,32 @@ TEST_CASE(link_profile_validation_rejects_invalid_values) {
     profile.bandwidthKbps = 0;
     profile.lossPercent = 101.0;
     ASSERT_TRUE(!profile.isValid());
+}
+
+TEST_CASE(link_profile_rejects_extreme_latency_and_jitter)
+{
+    LinkProfile profile{};
+
+    profile.latencyMs = std::numeric_limits<std::uint32_t>::max();
+    ASSERT_TRUE(!profile.isValid());
+
+    profile.latencyMs = 20;
+    profile.jitterMs = std::numeric_limits<std::uint32_t>::max();
+    ASSERT_TRUE(!profile.isValid());
+}
+
+TEST_CASE(virtual_network_rejects_invalid_extreme_delay_profile)
+{
+    LinkProfile profile{};
+    profile.latencyMs = std::numeric_limits<std::uint32_t>::max();
+    profile.jitterMs = std::numeric_limits<std::uint32_t>::max();
+
+    VirtualNetwork network(profile, 123);
+
+    Datagram datagram{};
+    datagram.fromNodeId = 1;
+    datagram.toNodeId = 2;
+    datagram.bytes = {1, 2, 3, 4};
+
+    ASSERT_TRUE(!network.submit(datagram, 0));
 }
